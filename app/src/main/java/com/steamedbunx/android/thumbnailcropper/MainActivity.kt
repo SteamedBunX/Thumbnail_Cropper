14ad65c40a4fdfb5775005be12ada0ea65ce31ba
Yi Hao <yi.hao01@outlook.com>
2019-10-11 15:05:12 -0500

    Initial commit

---
+++ app/src/main/java/com/steamedbunx/android/thumbnailcropper/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.steamedbunx.android.thumbnailcropper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.steamedbunx.android.thumbnailcropper.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/steamedbunx/android/thumbnailcropper/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.steamedbunx.android.thumbnailcropper.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
